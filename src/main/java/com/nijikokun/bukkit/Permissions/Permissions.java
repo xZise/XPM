2c1d9b6bb05a39b15ac6caff124e0d70ce0a5dfc
rcjrrjcr <ryanchewjingrong@yahoo.com.sg>
2011-05-27 18:30:55 +0800

    Update version numbers.

--- src/main/java/com/nijikokun/bukkit/Permissions/Permissions.java
+++ src/main/java/com/nijikokun/bukkit/Permissions/Permissions.java
@@ -55,7 +55,7 @@ public class Permissions extends JavaPlugin {
     public static Plugin instance;
     private Configuration storageConfig;
     public static final String name = "Permissions";
-    public static final String version = "3.0.4";
+    public static final String version = "3.0.5";
     public static final String codename = "Yeti";
 
     public Listener l = new Listener(this);
